fec26f3102f4c2ede9a393eba2a5e1c2412a21b7
Dabhi Urjit <urjitdabhi123@gmail.com>
2024-08-15 20:17:55 +0530

    Added todo component
    added some CSS and added methods

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import todo from './components/todo'
+import TODO from './components/todo'
 function App() {
 
   return (
     <>
-     
+     <TODO></TODO>
     </>
   )
 }
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,3 +1,25 @@
-export default todo = () => {
+import React,{useState} from "react"
+import './todo.css'
+const todo = () => {
+    const [todo, setTodo] = useState([]);
+    const [val,setVal] = useState('');
 
+    const addTodo = (e)=> {
+        if(val.trim()) {
+            setTodo(t=>[...t],e.target.value);
+            setVal("");
+        }
+    };
+    const deleteTodo = (index) => {
+        const newTodos = todo.filter((_, i) => i !== index);
+        setTodo(newTodos);
+      };
+    return(
+       <div className="App">
+        <h1>A simple to-do list</h1>
+        <input type="text" name="" id="" placeholder="enter a new Task" onChange={(e)=>setVal(e.target.value) } />
+        <button onClick={addTodo}></button>
+       </div>
+    )
 }
+export default todo;
