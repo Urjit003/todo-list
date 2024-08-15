199e0162c3a6d8058383105beb52f1be5b04f305
Dabhi Urjit <urjitdabhi123@gmail.com>
2024-08-15 20:23:22 +0530

    fixed todo
    add and delete works

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,25 +1,39 @@
-import React,{useState} from "react"
-import './todo.css'
+import React, { useState } from "react";
+import "./todo.css";
 const todo = () => {
-    const [todo, setTodo] = useState([]);
-    const [val,setVal] = useState('');
+  const [todo, setTodo] = useState([]);
+  const [val, setVal] = useState("");
 
-    const addTodo = (e)=> {
-        if(val.trim()) {
-            setTodo(t=>[...t],e.target.value);
-            setVal("");
-        }
-    };
-    const deleteTodo = (index) => {
-        const newTodos = todo.filter((_, i) => i !== index);
-        setTodo(newTodos);
-      };
-    return(
-       <div className="App">
-        <h1>A simple to-do list</h1>
-        <input type="text" name="" id="" placeholder="enter a new Task" onChange={(e)=>setVal(e.target.value) } />
-        <button onClick={addTodo}></button>
-       </div>
-    )
-}
+  const addTodo = (e) => {
+    if (val.trim()) {
+      setTodo((t) => [...t, val]);
+      setVal("");
+    }
+  };
+  const deleteTodo = (index) => {
+    const newTodos = todo.filter((_, i) => i !== index);
+    setTodo(newTodos);
+  };
+  return (
+    <div className="App">
+      <h1>A simple to-do list</h1>
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="enter a new Task"
+        onChange={(e) => setVal(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todo.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default todo;
